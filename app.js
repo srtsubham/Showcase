7d25c5202dba0e882d764058059753307214927e
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-09-02 21:35:15 +0530

    Major update. Performance and bug fixes.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             wheelMultiplier: 2.5,
             smoothWheel: true
         });
-        window.lns = a; //Update version 6.9
+        window.lns = a; //Update version 7.0.0
     }
 
     let b = 0;
