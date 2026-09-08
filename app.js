2a79ab37a1781032b9408ba09fd6bcc77f6aa40f
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-09-08 10:15:21 +0530

    Update version 8.7

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             wheelMultiplier: 2.5,
             smoothWheel: true
         });
-        window.lns = a; //Update version 8.6
+        window.lns = a; //Update version 8.7
     }
 
     let b = 0;
