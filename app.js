1126d9764f0bdd67604dd1543f00314c41f019d8
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-09-13 15:54:16 +0530

    Allocation module inpavement of version 6.1*

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             wheelMultiplier: 2.5,
             smoothWheel: true
         });
-        window.lns = a; //Update version 9.8
+        window.lns = a; //Update version 9.9
     }
 
     let b = 0;
