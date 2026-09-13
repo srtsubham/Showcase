822786061e6212fd4fc39582311413ca4995096d
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-09-13 15:53:51 +0530

    Update version 9.8 with secuirty and bug fixes.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             wheelMultiplier: 2.5,
             smoothWheel: true
         });
-        window.lns = a; //Update version 9.7
+        window.lns = a; //Update version 9.8
     }
 
     let b = 0;
