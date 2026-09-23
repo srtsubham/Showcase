fb8bba6511ece5981446d7a0a3eaab3907be94b6
Somya Ranjan Tripathi <somyar.tripathi@gmail.com>
2026-09-23 22:02:27 +0530

    Major stablization fixes and security updates.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             wheelMultiplier: 2.5,
             smoothWheel: true
         });
-        window.lns = a; //Update version 12.9
+        window.lns = a; //Update version 13.0
     }
 
     let b = 0;
